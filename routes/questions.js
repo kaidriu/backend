9299d7e6ff0046dd7c977f7761f0075c01b2916d
kaidriu <marcoastudillo.s@outlook.com>
2022-06-20 22:13:37 -0500

    natsu peticion

--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,7 @@ const { validarJWT } = require('../middlewares/validar-JWT');
 
 const router = Router();
 
-router.get('/get/:idc',[validarJWT,validarCampos],GetQuestion);
+router.get('/get/:idc', GetQuestion);
 router.post('/:idc',[validarJWT,validarCampos],PostQuestion);
 router.put('/put/:idq',[validarJWT,validarCampos],PutQuestion);
 router.delete('/delete/:idq',[validarJWT,validarCampos],DeleteQuestion);
